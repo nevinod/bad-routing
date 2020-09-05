6f335d8d601dc42d1143fe3e2a6f658bde62e936
Vinod <nevinod@38f9d325b51b.ant.amazon.com>
2020-09-05 14:58:29 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from './components/Home'
+import Comments from './components/Comments'
+import Posts from './components/Posts'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { home: true, comments: false, posts: false }
+
+  choosePage = (page) => {
+    switch(page) {
+      case 'home':
+        this.setState({home: true, comments: false, posts: false})
+        break;
+      case 'comments':
+        this.setState({home: false, comments: true, posts: false})
+        break;
+      case 'posts':
+        this.setState({home: false, comments: false, posts: true})
+    }
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <div style={{display: "flex", flexDirection: "row", cursor: "pointer", justifyContent: "space-between", width: "20%"}}>
+            <h4 onClick={() => this.choosePage('home')}>Home  </h4>
+            <h4 onClick={() => this.choosePage('comments')}>Comments  </h4>
+            <h4 onClick={() => this.choosePage('posts')}>Posts  </h4>
+          </div>
+
+
+          {this.state.comments && <Comments />}
+          {this.state.posts && <Posts />}
+          {this.state.home && <Home />}
+        </div>
+    );
+  }
 }
 
 export default App;
